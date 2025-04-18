4ffd8ace5396c34931b98ceeb54ef3b2a540afb3
Enzo <enzoframos10@gmail.com>
2025-04-17 21:41:07 -0300

    Questão 3- Cálculo do IMC com Switch case

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -18,3 +18,38 @@ console.log(garrafasVendidas != garrafasNecessarias);  // vai exibir 'true'
 console.log(garrafasVendidas == garrafasNecessarias);  // vai exibir 'false'
 console.log(garrafasVendidas >= garrafasNecessarias); // vai exibir 'true'
 console.log('--------------------------------------------')
+
+//Questão 3 - Cálculo do IMC com Switch case
+// Pergunta sobre o cliente
+console.log("Questão 3- Cálculo do IMC com Switch case")
+console.log("Será que o degustador da Vinharia Agnello está no peso ideal para apreciar nossos vinhos?");
+function calcularIMC(peso, altura) {
+    let imc = peso / (altura * altura);
+    let classificacao = "";
+
+    if (imc < 18.5) {
+        classificacao = "abaixo";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        classificacao = "ideal";
+    } else {
+        classificacao = "acima";
+    }
+
+    switch (classificacao) {
+        case "abaixo":
+            console.log(Seu IMC é ${imc}. Você está abaixo do peso);
+            break;
+        case "ideal":
+            console.log(Seu IMC é ${imc}. Você está no peso ideal);
+            break;
+        case "acima":
+            console.log(Seu IMC é ${imc}. Você está acima do peso);
+            break;
+        default:
+            console.log("Não foi possível calcular a categoria do IMC.");
+    }
+}
+
+// Exemplo de uso:
+calcularIMC(70, 1.75);
+console.log('--------------------------------------------')
