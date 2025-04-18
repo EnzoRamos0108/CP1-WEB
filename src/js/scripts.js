87db9e87c1974ad0f5d60ea0db4d32bcfe17721f
Felipe <fecerazi@gmail.com>
2025-04-17 22:07:27 -0300

    Questão 2 - Operadores !=, ==, >=

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,3 +7,14 @@ console.log(vinhoTinto);  // undefined
 console.log(vinhoBranco);  // vazio
 console.log(vinhoNome);  // Vinho Branco Chardonnay meio-seco
 console.log('--------------------------------------------')
+
+//Questão 2 - Operadores !=, ==, >=
+
+let garrafasVendidas = 60;
+let garrafasNecessarias = 50;
+
+console.log('Questão 2')
+console.log(garrafasVendidas != garrafasNecessarias);  // vai exibir 'true'
+console.log(garrafasVendidas == garrafasNecessarias);  // vai exibir 'false'
+console.log(garrafasVendidas >= garrafasNecessarias); // vai exibir 'true'
+console.log('--------------------------------------------')
