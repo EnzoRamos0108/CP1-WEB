2d6cd64bdc6596dd7f928b7538583ca52b0888b0
Gustavo <peagudagustavo@gmail.com>
2025-04-17 22:00:45 -0300

    Questão 1 - Declaração de variáveis

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+// Questão 1 - Declaração de variáveis
+let vinhoTinto;  // undefined
+let vinhoBranco = '';  // vazio
+let vinhoNome = 'Vinho Branco Chardonnay meio-seco';  // valor atribuído
+console.log('Questão 1')
+console.log(vinhoTinto);  // undefined
+console.log(vinhoBranco);  // vazio
+console.log(vinhoNome);  // Vinho Branco Chardonnay meio-seco
+console.log('--------------------------------------------')
